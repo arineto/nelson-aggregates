9c53c6e7ce755b0923b1231fd866acc1f3da4821
Arimatea Neto <arineto30@gmail.com>
2014-08-14 14:15:08 -0400

    Login picture on forgot password

--- core/views.py
+++ core/views.py
@@ -174,6 +174,10 @@ def access_info(request, username=None):
 
 def forgot_password(request):
 	answer = None
+	try:
+		login_picture = LoginPicture.objects.all().order_by('-id')[0]
+	except:
+		login_picture = None
 	if request.method == "POST":
 		email = request.POST.get("email")
 		try:
@@ -188,7 +192,7 @@ def forgot_password(request):
 		except:
 			answer = "There are no accounts registered in this email."
 
-	return render(request, 'login.html', {'forgot_password':True, 'answer':answer})
+	return render(request, 'login.html', {'forgot_password':True, 'answer':answer, 'login_picture':login_picture})
 
 
 @login_required
